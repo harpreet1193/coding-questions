2aabd1ea3f1c0d850c2f4805e3e92c38db1da342
Harpreet Singh Arora <harpreet.arora@leapfinance.com>
2023-12-03 15:34:08 +0530

    add kth smallest

---
+++ codingQuestions/src/main/java/com/example/codingquestions/trees/KthSmallestElement.java
@@ -0,0 +1,52 @@
+package com.example.codingquestions.trees;
+
+import com.sun.source.tree.Tree;
+
+import java.util.List;
+
+public class KthSmallestElement {
+
+    static int count = 0;
+
+    public static void main (String []args){
+        TreeNode root = null;
+        int keys[] = { 20, 8, 22, 4, 12, 10, 14 };
+        for (int x : keys)
+            root = insert(root, x);
+        System.out.println("1 Smallest " + kthSmallest(root, 1).val);
+        count = 0;
+        System.out.println("2 Smallest " + kthSmallest(root, 2).val);
+        count = 0;
+        System.out.println("3 Smallest " + kthSmallest(root, 3).val);
+        count = 0;
+        System.out.println("4 Smallest " + kthSmallest(root, 4).val);
+    }
+
+
+    public static TreeNode insert(TreeNode root, int x)
+    {
+        if (root == null)
+            return new TreeNode(x);
+        if (x < root.val)
+            root.left = insert(root.left, x);
+        else if (x > root.val)
+            root.right = insert(root.right, x);
+        return root;
+    }
+
+    public static TreeNode kthSmallest(TreeNode root, int k) {
+        if (root == null){
+            return null;
+        }
+        TreeNode left = kthSmallest(root.left, k);
+        if (left!=null) {
+            return left;
+        }
+        count++;
+        if(k==count) {
+            return root;
+        }
+        return kthSmallest(root.right, k);
+
+    }
+}
